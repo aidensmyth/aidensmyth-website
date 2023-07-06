139f693ae2e917abc1e0a5f63a45ba82341e771e
Aiden Smyth <57106231+aidensmyth@users.noreply.github.com>
2023-07-06 13:19:11 -0400

    Testing2

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,17 +1,12 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Aiden Smyth';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
-  const footerText = process.env.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
-
+    : 'Portfolio';
   return {
     name,
     blogTitle,
-    footerText,
   };
 };
